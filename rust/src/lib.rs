69bb3fa5489d464719a1c17e3026fec1183d4370
Justin Moon <mail@justinmoon.com>
2025-03-13 19:17:06 +0000

    some progress (broken) integrating macros to counter app

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,7 @@
-uniffi::setup_scaffolding!();
-
-// Module declarations
 mod app;
-mod view_model;
 
 // Re-exports for FFI
-pub use app::{Action, RmpModel};
-pub use view_model::{ModelUpdate, RmpViewModel};
+pub use app::{
+    Action,
+    ModelUpdate,
+};
